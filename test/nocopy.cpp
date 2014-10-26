429c28e4dee36638ee0f67ab7c51fcde07204e61
Christopher Kohlhoff <chris@kohlhoff.com>
2014-10-26 13:11:45 +1100

    Add test for noncopyable lambdas.

---
+++ test/nocopy.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+template <int N>
+struct noncopyable
+{
+  noncopyable() { printf("noncopyable()\n"); }
+  ~noncopyable() { printf("~noncopyable()\n"); }
+  noncopyable(const noncopyable&) = delete;
+};
+
+int main()
+{
+  auto&& f = [n = int(10)]() resumable
+  {
+    noncopyable<1> c1;
+
+    while (--n > 0)
+      if (n == 1) return n;
+      else yield n;
+  };
+
+  while (!f.is_terminal())
+    printf("%d\n", f());
+}
